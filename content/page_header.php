0e02197e4012b15e805b5a1ffb220d9ea7e19131
Sviatoslav <sv_dubov@ukr.net>
2021-07-15 20:03:38 +0300

    Made header and footer

---
+++ content/page_header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="utf-8">
+    <title><?php echo page_name(); ?></title>
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+    <link rel="stylesheet" type="text/css" href="styles/nav_bar.css">
+    <link rel="stylesheet" type="text/css" href="styles/goods.css">
+    <link rel="stylesheet" type="text/css" href="styles/profile.css">
+</head>
+<body>
